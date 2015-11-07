888f408b1aeb3c2814ddb47c535c6d9a9123979f
Krister Vals <krister.vals@gmail.com>
2015-11-07 22:40:33 +0200

    Improvements from the feedback

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-category = Category.create(name: "Cartoons")
+category = Category.create!(name: "Cartoons")
 
-Video.create(title: "Southpark", description: "Description of the Southpark video", small_cover_url: "/tmp/south_park.jpg",large_cover_url: "/tmp/south_park.jpg", category: category)
-Video.create(title: "Futurama", description: "Description of the Futurama video", small_cover_url: "/tmp/futurama.jpg",large_cover_url: "/tmp/futurama.jpg", category: category)
+Video.create!(description: "Description of the Southpark video", small_cover_url: "/tmp/south_park.jpg",large_cover_url: "/tmp/south_park.jpg", category: category)
+Video.create!(title: "Futurama", description: "Description of the Futurama video", small_cover_url: "/tmp/futurama.jpg",large_cover_url: "/tmp/futurama.jpg", category: category)
 
 
 
