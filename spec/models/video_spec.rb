2455d3f931fea278b469f8836d5b2fa3441cb3dc
Krister Vals <krister.vals@gmail.com>
2015-11-08 14:31:56 +0200

    Video required fields and test case

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -12,4 +12,22 @@ describe Video do
     video = Video.create(title: "Spiderman", description: "Spiderman cartoon from 2000", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/mong.jpg", category: category)
     expect(video.category).to eq(category)
   end
+
+  it "title is required" do
+    video = Video.new(description: "Spiderman cartoon from 2000")
+    video.valid?
+    expect(video.errors[:title].size).to eq(1)
+  end
+
+  it "description is required" do
+    video = Video.new(title: "Spiderman")
+    video.valid?
+    expect(video.errors[:description].size).to eq(1)
+  end
+
+  it "title and description is required" do
+    video = Video.new()
+    video.valid?
+    expect(video.errors.messages).to include(:title, :description)
+  end
 end
