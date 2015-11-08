d126bd3f2f628bd1319c580428ca9655208f6954
Krister Vals <krister.vals@gmail.com>
2015-11-08 15:04:52 +0200

    Replace test cases with shoulda-matchers

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,16 +1,5 @@
 require 'spec_helper'
 
 describe Category do
-  it "save Category record" do
-    category = Category.new(name: "Cartoons")
-    category.save
-    expect(Category.first).to eq(category)
-  end
-
-  it "has many videos" do
-    category = Category.create(name: "Cartoons")
-    spiderman = Video.create(title: "Spiderman", description: "Spiderman cartoon from 2000", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/mong.jpg", category: category)
-    ironman = Video.create(title: "Ironman", description: "Ironman cartoon from 2005", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/mong.jpg", category: category)
-    expect(category.videos).to include(spiderman, ironman)
-  end
+  it { should have_many(:videos) }
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,25 +1,7 @@
 require 'spec_helper'
 
 describe Video do
-  it "save Video record" do
-    video = Video.new(title: "Spiderman", description: "Spiderman cartoon from 2000", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/mong.jpg")
-    video.save
-    expect(Video.first).to eq(video) # The expect syntax is what the Rspec core team favors nowadays
-  end
-
-  it "has category" do
-    category = Category.create(name: "Cartoons")
-    video = Video.create(title: "Spiderman", description: "Spiderman cartoon from 2000", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/mong.jpg", category: category)
-    expect(video.category).to eq(category)
-  end
-
-  it "does not save without title" do
-    video = Video.create(description: "Spiderman cartoon from 2000")
-    expect(Video.count).to eq(0)
-  end
-
-  it "does not save without description" do
-    video = Video.create(title: "Spiderman")
-    expect(Video.count).to eq(0)
-  end
+   it { should validate_presence_of(:title) }
+   it { should validate_presence_of(:description) }
+   it { should belong_to(:category) }
 end
