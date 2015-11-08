499e9217a5bd5f0e83df02b855ce8989594e7e8d
Krister Vals <krister.vals@gmail.com>
2015-11-08 14:15:38 +0200

    Changing video and category test case from the solution

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -7,10 +7,10 @@ describe Category do
     expect(Category.first).to eq(category)
   end
 
-  it "have many videos" do
-    category = Category.create!(name: "Cartoons")
-    video1 = category.videos.create!(title: "Spiderman", description: "Spiderman cartoon from 2000", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/mong.jpg")
-    video2 = category.videos.create!(title: "Ironman", description: "Ironman cartoon from 2005", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/mong.jpg")
-    expect(category.reload.videos).to eq([video2, video1])
+  it "has many videos" do
+    category = Category.create(name: "Cartoons")
+    spiderman = Video.create(title: "Spiderman", description: "Spiderman cartoon from 2000", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/mong.jpg", category: category)
+    ironman = Video.create(title: "Ironman", description: "Ironman cartoon from 2005", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/mong.jpg", category: category)
+    expect(category.videos).to include(spiderman, ironman)
   end
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -7,10 +7,9 @@ describe Video do
     expect(Video.first).to eq(video) # The expect syntax is what the Rspec core team favors nowadays
   end
 
-  it "save Video with category" do
-    category = Category.first
-    video = Video.new(title: "Spiderman", description: "Spiderman cartoon from 2000", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/mong.jpg", category: category)
-    video.save
-    expect(video.reload.category).to eq(category)
+  it "has category" do
+    category = Category.create(name: "Cartoons")
+    video = Video.create(title: "Spiderman", description: "Spiderman cartoon from 2000", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/mong.jpg", category: category)
+    expect(video.category).to eq(category)
   end
 end
