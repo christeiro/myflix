2e5bf280ff9770c91161cac3a2810dccfc400d09
Krister Vals <krister.vals@gmail.com>
2015-11-11 16:26:46 +0200

    Users model, signup and route changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 Myflix::Application.routes.draw do
-  root to: 'users#index'
-  get 'ui(/:action)', controller: 'ui'
-  
-  get '/register', to: 'users#new'
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#register'
-
-  get 'home', to: 'videos#index'
+  root to: 'pages#front'
   resources :videos, only: [:show, :search] do
     collection do
       get 'search', to: 'videos#search'
     end
-  end
+  end  
+  get 'ui(/:action)', controller: 'ui'
+  get '/register', to: 'users#new'
+  get 'sign_in', to: 'sessions#new'
+  resources :users, only: [:create]
   resources :categories, only: [:show]
 end
