c212087af008a1a8edba57c1df13ffd9752cba51
Krister Vals <krister.vals@gmail.com>
2015-11-11 15:37:28 +0200

    Implementing the views and empty controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Myflix::Application.routes.draw do
+  root to: 'users#index'
   get 'ui(/:action)', controller: 'ui'
+  
+  get '/register', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#register'
+
   get 'home', to: 'videos#index'
   resources :videos, only: [:show, :search] do
     collection do
