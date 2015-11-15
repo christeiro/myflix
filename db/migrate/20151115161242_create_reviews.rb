d38f225e050ee4909cb60eb45e0b4d58cc2f5b55
Krister Vals <krister.vals@gmail.com>
2015-11-15 18:17:48 +0200

    Review model and migration

---
+++ db/migrate/20151115161242_create_reviews.rb
@@ -0,0 +1,10 @@
+class CreateReviews < ActiveRecord::Migration
+  def change
+    create_table :reviews do |t|
+      t.integer :rating
+      t.text :description
+      t.references :user, index: true, foreign_key: true
+      t.timestamps
+    end
+  end
+end
