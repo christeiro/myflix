bd6e4cc39eb66d366950b8b648cece5d3186867e
Krister Vals <krister.vals@gmail.com>
2015-11-18 21:42:50 +0200

    Add video to queue, order queue items and test cases

--- app/controllers/queue_items_controller.rb
+++ app/controllers/queue_items_controller.rb
@@ -1,6 +1,16 @@
 class QueueItemsController < ApplicationController
   before_action :require_user
   def index
-    @queue_items = current_user.queue_items
-  end  
+    @queue_items = current_user.queue_items.order("order_id ASC")
+  end
+
+  def create
+    queue_item = QueueItem.new(video_id: params[:video_id], user_id: current_user.id)
+    if queue_item.save
+      redirect_to my_queue_path
+    else 
+      flash[:error] = "Unable to add video to queue!"
+      redirect_to back
+    end
+  end
 end
--- spec/controllers/queue_items_controller_spec.rb
+++ spec/controllers/queue_items_controller_spec.rb
@@ -16,12 +16,40 @@ describe QueueItemsController do
         get :index
         expect(assigns(:queue_items)).to eq([queue_item])
       end
+      it "orders the @queue_items by order_id ASC" do
+        user = Fabricate(:user)
+        session[:user_id] = user.id
+        video1 = Fabricate(:video)
+        video2 = Fabricate(:video)
+        queue_item1 = Fabricate(:queue_item, video: video1, order_id: 2, user: user)
+        queue_item2 = Fabricate(:queue_item, video: video1, order_id: 1, user: user)
+        get :index
+        expect(assigns(:queue_items)).to start_with([queue_item2,queue_item1])
+      end
     end
     context "with unauthenticated user" do
       it "redirects to the root_path" do
         get :index
         expect(response).to redirect_to root_path
       end
+    end
+  end
+
+  describe "POST create" do
+    it "should redirect to root_path for unauthenticated user" do
+      post :create
+      expect(response).to redirect_to root_path
+    end
+    it "should redirect to my_queue path for authenticated user" do
+      session[:user_id] = Fabricate(:user)
+      post :create
+      expect(response).to redirect_to my_queue_path
+    end
+    it "should add a video to queue_items" do
+      session[:user_id] = Fabricate(:user)
+      video = Fabricate(:video)
+      post :create, video_id: video.id
+      expect(QueueItem.count).to eq(1)
     end
   end
 end
