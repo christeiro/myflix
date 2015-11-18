5ca34b80fe3dfa8f4b179e2497353092f0a4ab80
Krister Vals <krister.vals@gmail.com>
2015-11-18 18:48:56 +0200

    Inital QueueItem controller tests and index method in Controller

--- app/controllers/queue_items_controller.rb
+++ app/controllers/queue_items_controller.rb
@@ -1,3 +1,6 @@
 class QueueItemsController < ApplicationController
-  
+  before_action :require_user
+  def index
+    @queue_item = QueueItem.find_by(user_id: current_user)
+  end  
 end
---
+++ spec/controllers/queue_items_controller_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe QueueItemsController do
+  describe "GET index" do
+    context "with authenticated user" do
+      it "renders the index template" do
+        session[:user_id] = Fabricate(:user).id
+        get :index
+        expect(response).to render_template :index
+      end
+      it "sets the @queue_item variable" do
+        user = Fabricate(:user)
+        session[:user_id] = user.id
+        video = Fabricate(:video)
+        queue_item = Fabricate(:queue_item, video: video, user: user)
+        get :index
+        expect(assigns(:queue_item)).to eq(queue_item)
+      end
+    end
+    context "with unauthenticated user" do
+      it "redirects to the root_path" do
+        get :index
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
+end
