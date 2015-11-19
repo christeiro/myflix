a3917b97874921143d5b82d979fae3e4bf8591c8
Krister Vals <krister.vals@gmail.com>
2015-11-19 16:28:29 +0200

    Queue item changes from the feedback

--- app/controllers/queue_items_controller.rb
+++ app/controllers/queue_items_controller.rb
@@ -1,16 +1,26 @@
 class QueueItemsController < ApplicationController
   before_action :require_user
   def index
-    @queue_items = current_user.queue_items.order("order_id ASC")
+    @queue_items = current_user.queue_items.order("position ASC")
   end
 
   def create
-    queue_item = QueueItem.new(video_id: params[:video_id], user_id: current_user.id)
-    if queue_item.save
-      redirect_to my_queue_path
-    else 
-      flash[:error] = "Unable to add video to queue!"
-      redirect_to back
-    end
+    video = Video.find(params[:video_id])
+    queue_video(video)    
+    redirect_to my_queue_path
   end
+
+  private
+  def queue_video(video)
+    QueueItem.create(video: video, user: current_user, position: new_queue_item_position) unless current_user_queued_video?(video)
+  end
+
+  def new_queue_item_position
+    current_user.queue_items.count + 1
+  end
+
+  def current_user_queued_video?(video)
+    current_user.queue_items.map(&:video).include?(video)
+  end
+
 end
--- spec/controllers/queue_items_controller_spec.rb
+++ spec/controllers/queue_items_controller_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+ require 'spec_helper'
 
 describe QueueItemsController do
   describe "GET index" do
@@ -21,10 +21,10 @@ describe QueueItemsController do
         session[:user_id] = user.id
         video1 = Fabricate(:video)
         video2 = Fabricate(:video)
-        queue_item1 = Fabricate(:queue_item, video: video1, order_id: 2, user: user)
-        queue_item2 = Fabricate(:queue_item, video: video1, order_id: 1, user: user)
+        queue_item1 = Fabricate(:queue_item, video: video1, user: user)
+        queue_item2 = Fabricate(:queue_item, video: video1, user: user)
         get :index
-        expect(assigns(:queue_items)).to start_with([queue_item2,queue_item1])
+        expect(assigns(:queue_items)).to start_with([queue_item1,queue_item2])
       end
     end
     context "with unauthenticated user" do
@@ -36,20 +36,52 @@ describe QueueItemsController do
   end
 
   describe "POST create" do
-    it "should redirect to root_path for unauthenticated user" do
+    it "redirects to root_path for unauthenticated user" do
       post :create
       expect(response).to redirect_to root_path
-    end
-    it "should redirect to my_queue path for authenticated user" do
-      session[:user_id] = Fabricate(:user)
-      post :create
-      expect(response).to redirect_to my_queue_path
-    end
-    it "should add a video to queue_items" do
-      session[:user_id] = Fabricate(:user)
+    end 
+    it "redirects to my_queue path for authenticated user" do
+      session[:user_id] = Fabricate(:user).id
       video = Fabricate(:video)
       post :create, video_id: video.id
-      expect(QueueItem.count).to eq(1)
+      expect(response).to redirect_to my_queue_path
+    end
+    it "creates the queue item" do
+      session[:user_id] = Fabricate(:user).id
+      video = Fabricate(:video)
+      post :create, video_id: video.id
+      expect(QueueItem.count).to be(1)
+    end
+    it "creates the queue item that is associated with the video" do
+      session[:user_id] = Fabricate(:user).id
+      video = Fabricate(:video)
+      post :create, video_id: video.id
+      expect(QueueItem.first.video).to eq(video)
+    end
+    it "creates the queue item that is associated with the sign in user" do
+      user = Fabricate(:user)
+      session[:user_id] = user.id
+      video = Fabricate(:video)
+      post :create, video_id: video.id
+      expect(QueueItem.first.user).to eq(user)
+    end
+    it "puts the video as last one in the queue" do
+      user = Fabricate(:user)
+      session[:user_id] = user.id
+      monk = Fabricate(:video, title: "Monk")
+      Fabricate(:queue_item, video: monk, user: user)
+      futurama = Fabricate(:video, title: "Futurama")
+      post :create, video_id: futurama.id
+      futurama_queue_item = QueueItem.where(video_id: futurama.id, user_id: user.id).first
+      expect(futurama_queue_item.position).to eq(2)
+    end
+    it "does not add the video to the queue if the video is already in the queue" do
+      user = Fabricate(:user)
+      session[:user_id] = user.id
+      monk = Fabricate(:video, title: "Monk")
+      Fabricate(:queue_item, video: monk)
+      post :create, video_id: monk.id
+      expect(user.queue_items.count).to eq(1)
     end
   end
 end
